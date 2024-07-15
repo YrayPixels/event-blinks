d3bd99d69bde326e3f6c8cf2855f05627e0db76f
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-07-15 16:40:13 +0100

    add the get request to the page to avoid cors errors

--- src/app/api/developers/create/route.ts
+++ src/app/api/developers/create/route.ts
@@ -9,54 +9,62 @@ export const GET = (req: Request) => {
     try {
         const url = new URL(req.url);
         const params = new URLSearchParams(url.search);
-        const create = params.get('create');
 
-        console.log(create)
+        let productTitle = params.get('title');
+        let productDescription = params.get('description');
+        let productImage = params.get('image');
+        let productPrice = params.get('price');
+        let token = params.get('token');
+        let walletAddress = params.get('wallet');
 
-        if (!create) {
-            return new Response(JSON.stringify({ message: "No create parameter found" }), {
-                headers: ACTIONS_CORS_HEADERS,
-                status: 400
-            });
+        let missingFields = [];
+
+        if (!productTitle) missingFields.push("title");
+        if (!productDescription) missingFields.push("description");
+        if (!productImage) missingFields.push("image");
+        if (!productPrice) missingFields.push("price");
+        if (!token) missingFields.push("token");
+        if (!walletAddress) missingFields.push("walletAddress");
+
+        if (missingFields.length > 0) {
+            return Response.json({ message: `Missing required fields: ${missingFields.join(", ")}` }, { status: 400, headers: ACTIONS_CORS_HEADERS });
         }
 
-        let adjusted = JSON.stringify(JSON.parse(create).code.replace(/ /g, '+'));
+
+        let blinkJson = {
+            "title": productTitle,
+            "description": productDescription,
+            "image": productImage,
+            "price": productPrice,
+            "actionTitle": `Purchase with ${productPrice} ${token}}`,
+            "actionUrl": "https://www.quick-blinks.xyz/api/actions/mint?send=" + encodeURIComponent(JSON.stringify({
+                link: "https://www.quick-blinks.xyz/api/actions/mint",
+                walletAddress: walletAddress,
+                price: productPrice
+            })),
+            "walletAddress": walletAddress,
+        }
+
+        //to Json String
+        const jsonString = JSON.stringify(blinkJson);
+        //To Gzip
+        const compressed = pako.gzip(jsonString);
+        //To Base64
+        const base64Encoded = Buffer.from(compressed).toString('base64');
+
+        let item = JSON.stringify({ code: base64Encoded });
 
 
-        // Step 1: Decode base64 to buffer
-        const compressedRe = Buffer.from(adjusted, 'base64');
+        let paymentLink = `https://www.dial.to/?action=solana-action:https://www.quick-blinks.xyz/api/actions/mint?create=${item}`;
 
-        // Step 2: Unzip the buffer
-        const decompressedRe = pako.ungzip(compressedRe);
+        let response = {
+            paymentLink: paymentLink,
+            message: "Success"
+        }
 
-        // Step 3: Convert buffer to string
-        const jsonStringRe = Buffer.from(decompressedRe).toString('utf-8');
-
-        // Step 4: Parse JSON string
-        const item = JSON.parse(jsonStringRe);
-
-        console.log(item);
+        return Response.json(response, { status: 200, headers: ACTIONS_CORS_HEADERS })
 
 
-
-        const payload = {
-            icon: new URL(item.image, url.origin).toString(),
-            title: item.title,
-            description: item.description,
-            label: `${item.actionTitle}`,
-            links: {
-                actions: [
-                    {
-                        href: `${item.actionUrl}`,
-                        label: `${item.actionTitle}`
-                    }
-                ]
-            }
-        };
-
-        return new Response(JSON.stringify(payload), {
-            headers: ACTIONS_CORS_HEADERS
-        });
     } catch (error: any) {
         return new Response(JSON.stringify({ message: "Error parsing request", error: error?.response?.message }), {
             headers: ACTIONS_CORS_HEADERS,

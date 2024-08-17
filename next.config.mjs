d3723f17e8c98c2234e4b679dac7b3f8411474d6
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-08-17 09:08:33 +0100

    updated the

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    reactStrictMode: true,
-    images: { unoptimized: true },
-    // webpack: (config, { dev }) => {
-    //     if (dev) {
-    //         Object.defineProperty(config, 'devtool', {
-    //             get() {
-    //                 return "cheap-source-map";
-    //             },
-    //             set() { },
-    //         });
-    //     }
-    //     return config;
-    // },
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;

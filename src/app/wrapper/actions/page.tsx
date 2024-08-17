896d8c0b014d7316bee2dde552007ff532b3c3eb
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-08-17 07:21:58 +0100

    upated the next config according to hosting specs

--- src/app/wrapper/actions/page.tsx
+++ src/app/wrapper/actions/page.tsx
@@ -1,19 +1,54 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
+import '@dialectlabs/blinks/index.css';
+import { Action, Blink, ActionsRegistry, useAction } from "@dialectlabs/blinks";
+import { useActionSolanaWalletAdapter } from "@dialectlabs/blinks/hooks/solana"
+import { Connection } from '@solana/web3.js';
+import { NETWORK } from '@/app/utils/requestsHandler';
+
+import { CanvasClient, CanvasInterface } from '@dscvr-one/canvas-client-sdk';
+import { registerCanvasWallet } from '@dscvr-one/canvas-wallet-adapter';
 
 const BlinksWrapper = () => {
+
+
+
+
+    // const [action, setAction] = useState<Action | null>(null);
+    const connection = new Connection(NETWORK, "confirmed");
+
+    // const { adapter } = useActionSolanaWalletAdapter(connection);
+    const actionApiUrl = 'https://www.quick-blinks.xyz/api/events/create';
+    // const { action } = useAction({ url: actionApiUrl, adapter });
+
+    useEffect(() => {
+        (async () => {
+            const canvasClient = new CanvasClient();
+            registerCanvasWallet(canvasClient);
+            const response = await canvasClient.ready();
+            console.log('Canvas client ready', response);
+
+            if (response) {
+                if ("Handshake" in CanvasInterface) {
+                    // The handshake allows access to the user and the content that the application is embedded in.
+                    const user: CanvasInterface.Handshake.User = response.untrusted.user;
+                    const content: CanvasInterface.Handshake.Content = response.untrusted.content;
+                    // ...
+                }
+            }
+
+        })()
+    }, [])
     return (
         <div className='bg-[url("/grid_bg.png")] h-screen flex flex-row justify-center items-center'>
-            <div className=''> <button className='bg-white hover:bg-white/50 hover:text-white text-black p-2 shadow-lg rounded-xl'>
+            <div className='absolute top-5 right-10'> <button className='bg-white hover:bg-white/50 hover:text-white text-black p-2 shadow-lg rounded-xl'>
                 Connect Dscvr
             </button></div>
             <div className='flex flex-col justify-center items-center space-y-6'>
 
-                <h1 className='text-[40px]'>Import your existing blinks with a click!</h1>
 
-                <input placeholder='paste your action get url' className='p-2 text-center border-white w-8/12  border rounded-xl  bg-black/10' />
-                <button className='bg-white hover:bg-white/50 hover:text-white text-black p-2 shadow-lg rounded-xl'>
-                    Generate
-                </button>
+                {/* {action ? <Blink action={action} websiteText={new URL(actionApiUrl).hostname} /> : null} */}
+
             </div>
 
         </div>

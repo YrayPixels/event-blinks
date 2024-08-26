31f1faeb3e73ea8881c85b41c477be108957496b
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-08-26 08:47:29 +0100

    pushed dscvrtrivia

--- src/app/api/dscvr-quiz/route.ts
+++ src/app/api/dscvr-quiz/route.ts
@@ -6,65 +6,21 @@ import { Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, cl
 export const GET = (req: Request) => {
     try {
         const url = new URL(req.url);
+
+
         const payload = {
             icon: `${process.env.NEXT_PUBLIC_HOST_URL}/events.png`,
-            title: "Create Quick Events With Blinks",
-            description: "Create and manage your events and events Items directly from blinks, get access to the number of those that have purchased your tickets and registered for your events directly from your blinks.",
-            label: `Create Event`,
-
+            title: "Daily DSCVR Quiz",
+            description: "",
             links: {
                 actions: [
                     {
-                        href: `/api/events/create`,
+                        href: `/api/dscvr-quiz`,
                         label: 'Create Event',
                         "parameters": [
                             {
-                                "name": "email_address", // field name
-                                "label": "enter your email address", // text input placeholder
-                                type: "email",
-                                required: true,
-
-                            },
-                            {
-                                "name": "event_name", // field name
-                                "label": "enter name/title for your event", // text input placeholder
-                                type: "text",
-                                required: true,
-
-                            },
-                            {
-                                name: "description", //
-                                label: 'enter a short description of the event', // text input placeholder,
-                                type: "textarea",
-                                required: true,
-
-                            },
-                            {
-                                name: "date_time",
-                                label: 'enter event date in the format YYYY-MM-DD', // date input placeholder
-                                type: "datetime-local",
-                                required: true,
-
-                            },
-                            {
-                                name: "location",
-                                label: 'enter event location', // text input placeholder
-                                type: "text",
-                                required: true,
-                            },
-                            {
-                                name: "flyer_url",
-                                label: 'enter the link to the flyer for your event - leave empty if none', // text input placeholder
-                                type: "url",
-                            },
-                            {
-                                name: "fee",
-                                label: 'Enter Fee Leave empty if it is free', // text input placeholder
-                                type: "number",
-                            },
-                            {
-                                name: "payment_token",
-                                label: 'Select Payment Token', // text input placeholder
+                                name: "answer",
+                                label: 'Choose from the options below', // text input placeholder
                                 type: "radio",
                                 options: [
                                     {
@@ -77,15 +33,12 @@ export const GET = (req: Request) => {
                                     },
                                 ]
                             },
-                            {
-                                name: "payment_address",
-                                label: 'Enter Address You want to receive payment to', // text input placeholder
-                                type: "text",
-                            },
                         ],
 
                     }
-                ]
+                ],
+                next: '/events/create',
+
             }
         };
 
@@ -115,7 +68,7 @@ export const POST = async (req: Request) => {
 
         let address = process.env.WALLET_ADDRESS || "13dqNw1su2UTYPVvqP6ahV8oHtghvoe2k2czkrx9uWJZ";
         let walletAddress = new PublicKey(address);
-        const lamportsToSend = Number(0.005) * LAMPORTS_PER_SOL;
+        const lamportsToSend = Number(0.00001) * LAMPORTS_PER_SOL;
 
         const transferTransaction = new Transaction().add(
             SystemProgram.transfer({
@@ -128,34 +81,34 @@ export const POST = async (req: Request) => {
         const connection = new Connection(NETWORK);
         transferTransaction.feePayer = new PublicKey(body.account);
         transferTransaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
-        const date = new Date(data?.date_time);
-        const hours = date.getHours().toString().padStart(2, '0');
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        // Format time as "HH:MM"
-        const time = `${hours}:${minutes}`
+        // const date = new Date(data?.date_time);
+        // const hours = date.getHours().toString().padStart(2, '0');
+        // const minutes = date.getMinutes().toString().padStart(2, '0');
+        // // Format time as "HH:MM"
+        // const time = `${hours}:${minutes}`
 
-        try {
-            const response = await createEvent(
-                data?.event_name,
-                data?.description,
-                data?.date_time,
-                data?.location,
-                data?.flyer_url,
-                time,
-                data?.payment_token,
-                data?.payment_address,
-                body.account,
-                data?.fee,
-                data?.email_address,
-            );
+        // try {
+        //     const response = await createEvent(
+        //         data?.event_name,
+        //         data?.description,
+        //         data?.date_time,
+        //         data?.location,
+        //         data?.flyer_url,
+        //         time,
+        //         data?.payment_token,
+        //         data?.payment_address,
+        //         body.account,
+        //         data?.fee,
+        //         data?.email_address,
+        //     );
 
-        } catch (eventError: any) {
-            console.error('Error in createEvent:', eventError);
-            const error: ActionError = {
-                message: `${eventError.message}`,
-            }
-            return Response.json(error, { status: 400, headers: ACTIONS_CORS_HEADERS })
-        }
+        // } catch (eventError: any) {
+        //     console.error('Error in createEvent:', eventError);
+        //     const error: ActionError = {
+        //         message: `${eventError.message}`,
+        //     }
+        //     return Response.json(error, { status: 400, headers: ACTIONS_CORS_HEADERS })
+        // }
         const payload: ActionPostResponse = await createPostResponse({
             fields: {
                 transaction: transferTransaction,
